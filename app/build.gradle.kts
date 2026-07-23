e8102d3e254447a825a6e08850a3ac6e026cb11a
eepyminded <papdfer@protonmail.com>
2026-07-23 23:50:08 +0200

    Set version to 0.0.3 for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.repeatersmap"
         minSdk = 27
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
